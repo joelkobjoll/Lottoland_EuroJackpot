a9b57d47a0ebdb0678170d8c4efc980bd440a60c
joelkobjoll <kobjoll@gmail.com>
2016-10-05 20:16:55 +0200

    JSONP Method
    Ajax jsonp call method

---
+++ media/ts/partials/jsonp.ts
@@ -0,0 +1,26 @@
+export var getJSONP = function(url: any) {
+	let id: number = 0;
+
+	return new Promise(function(resolved, rejected) {
+		let callback = '_jsonp_' + id++;
+
+		(window as any)[callback] = function(data: any) {
+			let element: any = document.getElementById(callback);
+
+			element.parentNode.removeChild(element);
+
+			delete (window as any)[callback];
+
+			resolved(data);
+		};
+
+		let src: string = url + (url.indexOf('?') > 0 ? '&' : '?') + 'callback=' + callback;
+		let script: any = document.createElement('script');
+
+		script.src = src;
+		script.id = callback;
+		script.addEventListener('error', rejected);
+
+		document.getElementsByTagName('head')[0].appendChild(script);
+	});
+};
